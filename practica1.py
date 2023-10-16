0740d8edf580767e12c643054dddcc01ebe7ab57
MizaelS <sansoresangel97@gmail.com>
2023-10-16 10:47:55 -0600

    Subida de archivos

---
+++ practica1.py
@@ -0,0 +1,32 @@
+#-----------------------------------------#
+# Nombre: Mizael S.                       #
+# Fecha: 16/10/2023                       #
+#-----------------------------------------#
+
+def evaluar_caracter(caracter):
+    # Convertidos el caracter a minuscula para evitar falsos
+    caracter = caracter.lower()
+    # Lissta de vocales
+    vocales = ['a', 'e', 'i', 'o', 'u']
+
+    # Verificamos si el caracter es una vocal
+    if caracter in vocales:
+        return "✅ Verdadero, el caracter es una vocal."
+    # Se verifica si el caracter es alfabetico y no es una vocal entonces es una consonante
+    elif caracter.isalpha():
+        return "❌ El caracter es una consonante."
+    # Si no es alfabecito entonces es un signo
+    else:
+        return "❌ El caracter es un signo."
+     
+    
+# Obtener el caracter que ingrese el usuario 
+caracter = input("- Introduce el caracter a evaluar: ")
+
+# Pasar los datos a la función
+resultado = evaluar_caracter(caracter)
+
+# Imprimir resultados
+print(f"{resultado}")
+
+
---
+++ problema8.py
@@ -0,0 +1,19 @@
+#-----------------------------------------#
+# Nombre: Angel Mizael Segovia Sansores   #
+# Fecha: 03/10/2023                       #
+#-----------------------------------------#
+
+# Definir la función para calcular el tiempo de encuentro
+def tiempo_encuentro(v1, v2, distancia):
+    return distancia / (v1 + v2)
+
+# Obtener los datos necesarios para evaluar la formaula
+v1 = float(input("- Introduce la velocidad del primer cuerpo: "))
+v2 = float(input("- Introduce la velocidad del segundo cuerpo: "))
+distancia = float(input("- Introduce la distancia inicial que los separa: "))
+
+# Llamar a la función con los datos recolectados
+t = tiempo_encuentro(v1, v2, distancia)
+
+# Imprimir resultados
+print(f"- El tiempo de encuentro es: {t} unidades de tiempo")
