b61aff1a7995c285042481072ef2db835dd51272
MizaelS <sansoresangel97@gmail.com>
2023-10-20 10:55:27 -0600

    modified:   Extras/Calculador de Propinas/calculador_de_propinas.py

--- Extras/Calculador de Propinas/calculador_de_propinas.py
+++ Extras/Calculador de Propinas/calculador_de_propinas.py
@@ -10,7 +10,7 @@ subtotal = float(input("Bienvenido, por favor, ingrese el monto de la factura to
 # Pedir al usuario que % de propina quiere dejar
 porcentaje_propina = float(input(f"Ingrese el % de propina que desea dejar para el total de ${subtotal}: "))
 
-# Calcular el total de propina en base al porcentaje que ingreso el usuario
+# Calcular el total de propina en base al porcentaje que ingreso el usuario 
 propina = subtotal * (porcentaje_propina / 100)
 
 # Calcular el total de la cuenta (subtotal + propina)
