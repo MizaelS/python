30df450c0cead2d4680a23a114780bbe2e465aa7
MizaelS <sansoresangel97@gmail.com>
2023-10-23 10:38:09 -0600

    modified:   Practica 3/ejercicio5.py

--- Practica 3/ejercicio5.py
+++ Practica 3/ejercicio5.py
@@ -3,14 +3,14 @@
 # Fecha: 23/10/2023                       #
 #-----------------------------------------#
 
-# Almacenar la cadena de caracteres en una variable
-password = "test123"
+# Guardamos la contraseña en una variable
+password = "cadena"
 
 # Iniciar un buble while para solicitar la contraseña al usuario hasta que sea correcta
 while True:
     contraseña_ingresada = input("Ingrese la contraseña: ")
     if contraseña_ingresada == password:
         print("Contraseña correcta. Acceso concedido.")
-        break  # Salir del bucle si la contraseña es correcta
+        break  # Se sale del buble si la contraseña es correcta
     else:
         print("Contraseña incorrecta. Intente nuevamente.")
