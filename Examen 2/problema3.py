b550142c6a8b72a59cf639d2cc3f82c986e49651
MizaelS <sansoresangel97@gmail.com>
2023-10-25 20:34:26 -0600

    modified:   Examen 2/problema3.py

--- Examen 2/problema3.py
+++ Examen 2/problema3.py
@@ -3,3 +3,9 @@
 # Fecha: 23/10/2023                       #
 # Problema: 3                             #
 #-----------------------------------------#
+
+# Declaramos frase
+frase = input("- Ingrese la palabra: ")
+letras = frase.split()
+
+print(letras)
